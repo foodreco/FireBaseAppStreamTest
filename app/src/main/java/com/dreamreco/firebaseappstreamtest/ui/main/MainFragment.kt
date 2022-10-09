d6c8ec2a350b170a42c7ab87bfeada1db6a8bbdb
Jake Lee <huny357@naver.com>
2022-10-09 17:50:26 +0900

    1. firebase 만 추가한 상태 2. 로그 작동 이상무

--- app/src/main/java/com/dreamreco/firebaseappstreamtest/ui/main/MainFragment.kt
+++ app/src/main/java/com/dreamreco/firebaseappstreamtest/ui/main/MainFragment.kt
@@ -6,21 +6,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import com.dreamreco.firebaseappstreamtest.R
 import com.dreamreco.firebaseappstreamtest.databinding.FragmentMainBinding
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainFragment : Fragment() {
 
     private val binding by lazy { FragmentMainBinding.inflate(layoutInflater) }
     private val mainViewModel by viewModels<MainViewModel>()
+    private lateinit var mFirebaseAnalytics : FirebaseAnalytics
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        binding.btnEvent1.setOnClickListener {
+            activateFirebase()
+        }
+
         // nav 세팅 완료됨
         return binding.root
     }
 
+    private fun activateFirebase() {
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(requireContext())
+        val bundle = Bundle()
+        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "테스트아이디")
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "보내는이름")
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "이건타입")
+
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle)
+        Toast.makeText(requireContext(), "이벤트 전송함", Toast.LENGTH_SHORT).show()
+    }
 }
